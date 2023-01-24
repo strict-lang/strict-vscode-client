6d88bc9a20f31947044044ecb09b54afc134376d
Lukaa <lukaminjoraia@gmail.com>
2023-01-24 15:55:16 +0100

    scrunch initial

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 import { connect } from "net";
-import { window as Window } from 'vscode';
-
-
+import path = require("path");
+import { Range, Uri, window as Window, TextEditorDecorationType} from 'vscode';
 import {
   LanguageClient,
   StreamInfo,
-  CancellationStrategy
+  CancellationStrategy,
+  integer,
 } from "vscode-languageclient/node";
 
 let client: LanguageClient;
@@ -46,8 +46,8 @@ export function activate() {
         quickPick.title = 'Enter methodcall :';
         quickPick.items = choices.map(choice => ({ label: choice }));
         quickPick.onDidChangeValue(() => {
-          if (!choices.includes(quickPick.value)) { 
-            quickPick.items = [quickPick.value, ...choices].map(label => ({ label })); 
+          if (!choices.includes(quickPick.value)) {
+            quickPick.items = [quickPick.value, ...choices].map(label => ({ label }));
           }
         });
 
@@ -60,6 +60,32 @@ export function activate() {
           return next(command, args);
         });
         quickPick.show();
+      }
+    }
+  });
+  let green = Uri.parse('https://www.clipartmax.com/png/middle/1-14437_green-smiley-face-clip-art-thumbs-up-emoji-green.png'); //use actual icons
+  let red = Uri.parse('https://www.clipartmax.com/png/small/438-4384778_emoji-anger-red-face.png'); //use actual icons
+  let decorationTypes = new Map<integer, TextEditorDecorationType>();
+  client.onNotification('testRunnerNotification', (testMessage: any) => {
+    var image = testMessage.state === 0 ? red : green;
+    var position = new Range(testMessage.lineNumber, 0, testMessage.lineNumber, 0);
+    let decorationType = Window.createTextEditorDecorationType({
+      gutterIconPath: image,
+      gutterIconSize: 'contain'
+    });
+    const editor = Window.activeTextEditor;
+    if (editor !== undefined) {
+      var possibleDecoration = decorationTypes.get(testMessage.lineNumber);
+      if (possibleDecoration === undefined) {
+        decorationTypes.set(testMessage.lineNumber, decorationType);
+        editor.setDecorations(decorationType, [{
+          range: position
+        }]);
+      }
+      else {
+        editor.setDecorations(possibleDecoration, []);
+        decorationTypes.delete(testMessage.lineNumber);
+        editor.setDecorations(decorationType, [{ range: position }]);
       }
     }
   });
